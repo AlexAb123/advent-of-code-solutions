6fde61bde4f02b59e6d3ed03c13e141fbbb54aa0
AlexAb123 <AlexAb123@users.noreply.github.com>
2025-12-04 05:23:45 +0000

    2025 day04

--- aoc2025/day04.py
+++ aoc2025/day04.py
@@ -1,12 +1,43 @@
 def solve(input_path):
 
-    lines = input_path.open('r').read().strip().split("\n")
-
-    part1 = 0
-    part2 = 0
-
+    lines = list(map(list, input_path.open('r').read().strip().split("\n")))
     
+    def get_adjs(r, c, lines):
+          for dr in -1, 0, 1:
+            for dc in -1, 0, 1:
+                if dr == dc == 0:
+                    continue
+                if not (0 <= r + dr < len(lines) and (0 <= c + dc < len(lines[0]))):
+                    continue
+                if lines[r + dr][c + dc] != "@":
+                    continue
+                yield (r + dr, c + dc)
 
+    adjs_count = {}
+    for r in range(len(lines)):
+        for c in range(len(lines[0])):
+            if lines[r][c] != "@":
+                continue
+            adjs_count[(r, c)] = len(list(get_adjs(r, c, lines)))
+
+    q = []
+    for (r, c), count in adjs_count.items():
+        if count < 4:
+            q.append((r, c))
+
+    part1 = len(q)
+    seen = set(q)
+    while q:
+        r, c = q.pop(0)
+        for adj in get_adjs(r, c, lines):
+            if adj in seen:
+                continue
+            adjs_count[adj] -= 1
+            if adjs_count[adj] < 4:
+                q.append(adj)
+                seen.add(adj)
+    part2 = len(seen)
+    
     return part1, part2
 
 if __name__ == "__main__":
