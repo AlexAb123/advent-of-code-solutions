8e36a235b15c3accf352081de789f406e25d7715
AlexAb123 <AlexAb123@users.noreply.github.com>
2025-12-08 04:36:01 +0000

    2025 day8

--- aoc2025/day07.py
+++ aoc2025/day07.py
@@ -1,6 +1,6 @@
-from functools import lru_cache
-def solve(data):
+from functools import cache
 
+def solve(data):
     lines = data.split("\n")
 
     splits = set()
@@ -10,29 +10,29 @@ def solve(data):
                 start = (r, c)
             elif lines[r][c] == "^":
                 splits.add((r, c))
-                
+
     global part1
     part1 = 0
-    @lru_cache(maxsize=None)
+    @cache
     def timelines(r, c):
         if r >= len(lines):
             return 1
         if (r, c) in splits:
             global part1
             part1 += 1
-            return timelines(r, c+1) + timelines(r, c-1)
-        else:
-            return timelines(r+1, c)
-        
+            return timelines(r, c + 1) + timelines(r, c - 1)
+        return timelines(r + 1, c)
+
     part2 = timelines(*start)
-    
+
     return part1, part2
-    
+
+
 if __name__ == "__main__":
     from pathlib import Path
     from time import time
-    data = (Path(__file__).parent/"inputs"/"day07.txt").read_text().strip()
+    data = (Path(__file__).parent / "inputs" / "day07.txt").read_text().strip()
     start = time()
     part1, part2 = solve(data)
     print(f"Part 1: {part1}\nPart 2: {part2}")
-    print(f"Time Taken: {(time() - start)*1000:.2f} ms")
+    print(f"Time Taken: {(time() - start) * 1000:.2f} ms")
