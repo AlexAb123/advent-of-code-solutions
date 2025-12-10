1836f7a97b66fd7fbe6cd09ade67cb2c02e897c2
AlexAb123 <AlexAb123@users.noreply.github.com>
2025-12-10 17:24:01 +0000

    day10 p2 scipy

--- aoc2025/day10.py
+++ aoc2025/day10.py
@@ -1,8 +1,16 @@
 from heapq import heappush, heappop
 from functools import cache
+import numpy as np
+from scipy.optimize import minimize, milp, linprog
 
 def solve(data):
 
+    def transform_button(button, length):
+        new_button = [0 for _ in range(length)]
+        for i in button:
+            new_button[i] = 1
+        return new_button
+        
     lines = list(map(lambda x: x.split(" "), data.split("\n")))
 
     part1 = part2 = 0
@@ -11,9 +19,11 @@ def solve(data):
 
     for line in lines:
         target = line[0][1:-1]
-        buttons = set()
+        buttons = []
         for button in line[1:-1]:
-            buttons.add(frozenset(map(int, button[1:-1].split(","))))
+            button = list(map(int, button[1:-1].split(",")))
+            button = transform_button(button, len(target))
+            buttons.append(np.array(button, dtype=int))
         joltage = tuple(map(int, line[-1][1:-1].split(",")))
         machines.append((target, buttons, joltage))
     def adjs1(curr, buttons):
@@ -94,15 +104,47 @@ def solve(data):
                     heappush(q, (dists[adj] + h(adj, target), counter, adj))
         return float("inf")
         
+    def f(xs: np.ndarray):
+        return int(np.sum(xs))
+    def grad_f(xs: np.ndarray):
+        return np.array(1 for _ in range(len(xs)))
+        
+    def g(xs: np.ndarray, buttons: list[np.ndarray], target: np.ndarray):
+        total = target.copy()
+        for i in range(len(xs)):
+            x = xs[i]
+            button = buttons[i]
+            total = total - x * button
+        return total
+        
+        
+
     #print(a_star((182, 180, 23, 44, 170, 66), (198, 181, 22, 50, 173, 65), {frozenset({0, 2, 4, 5}), frozenset({1, 3, 5}), frozenset({0, 1, 4}), frozenset({2, 3}), frozenset({4, 5}), frozenset({0, 2, 3, 5}), frozenset({0, 5}), frozenset({0, 3, 5})}))
     for machine in machines:
-        print(machine)
-        #part1 += bfs1("." * len(machine[0]), machine[0], machine[1])
+        #print(machine)
+        part1 += bfs1("." * len(machine[0]), machine[0], machine[1])
         #part2 += dfs(machine[2], tuple(machine[1]))
-        part2 += a_star(tuple([0 for _ in range(len(machine[2]))]), machine[2], machine[1])
+        #part2 += a_star(tuple([0 for _ in range(len(machine[2]))]), machine[2], machine[1])
         
+        
+        
+        buttons = machine[1]
+        
+        c = np.array([1 for _ in range(len(buttons))]) # Minimize c @ xs. This (array of 1s) would minimize the sum of xs
+        integrality = np.array([1 for _ in range(len(buttons))]) # All xs must be integers
+        # Transpose so that each button is its own column because ecah button is associated with 
+        # one x (the number of times to press that specific button)
+        A_eq = np.array([button for button in buttons]).T 
+        for b in A_eq:
+            print(b)
+        b_eq = np.array(machine[2], dtype=int)
+        bounds = [(0, None) for _ in range(len(c))]
+        #A_eq = ({'type': 'eq', 'fun': lambda xs: g(xs, buttons, target)})
+        res = linprog(c, A_eq=A_eq, b_eq=b_eq, bounds=bounds, integrality=integrality)
+        part2 += res.fun
+    part2 = int(part2)
     return part1, part2
-
+# 11860 too low, 20082 too low
 if __name__ == "__main__":
     from pathlib import Path
     from time import time
